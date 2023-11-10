406727783c5a0510e8092fbeb278eaa514f1f0db
Jerry <s724052429@gmail.com>
2023-11-11 04:42:55 +1100

    add new cnn network

--- Comp9517_Suring_GroupWork.py
+++ Comp9517_Suring_GroupWork.py
@@ -1,138 +1,152 @@
 import numpy as np
+import tensorflow as tf
 import keras
 import cv2
+import random
 from utils.elpv_reader import load_dataset
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-from keras.optimizers import SGD
+from keras.optimizers import SGD,Adam
 from keras.models import Sequential
-from keras.layers import Dense,Dropout,Flatten,Activation,Conv2D, MaxPooling2D
-from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense,Dropout,Flatten,Activation,Conv2D, MaxPooling2D,GlobalMaxPooling2D,BatchNormalization
+from sklearn.model_selection import KFold
 from keras.callbacks import Callback
 from keras.utils import to_categorical
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score,f1_score
-from sklearn.preprocessing import label_binarize
+from sklearn.utils.class_weight import compute_class_weight
+from focal_loss import SparseCategoricalFocalLoss
+from keras.regularizers import l2
+from keras.callbacks import ModelCheckpoint
+from keras.models import load_model
 
-# Respectively: image, reflection probability, type. Our classifier needs to classify based on reflection probability
+best_model = load_model('best_model.h5')
+
 images, proba, types = load_dataset()
-labels = np.unique(proba)
-type1 = 0.0
-type2 = 0.3333333333333333
-type3 = 0.6666666666666666
-type4 = 1.0
+# change label, label will be 0 1 2 3
+label_mapping = {0.0: 0, 0.3333333333333333: 1, 0.6666666666666666: 2, 1.0: 3}
 for x in range(len(proba)):
-    if proba[x] == type1:
-        proba[x] = 0
-        proba[x] = proba[x].astype(int)
-        continue
-    if proba[x] == type2:
-        proba[x] = 1
-        proba[x] = proba[x].astype(int)
-        continue
-    if proba[x] == type3:
-        proba[x] = 2
-        proba[x] = proba[x].astype(int)
-    if proba[x] == type4:
-        proba[x] = 3
-        proba[x] = proba[x].astype(int)
+    if proba[x] in label_mapping:
+        proba[x] = label_mapping[proba[x]]
 # get the infomations of images
 num_images, height, width = images.shape
 # Create a new numpy array to store the resized and denoised images
-resized_images = np.empty((num_images, 128, 128), dtype=np.float32)
+resized_images = np.empty((num_images, 64, 64), dtype=np.uint8)
 # use for loop, resize every image and denoise it
 for i in range(num_images):
-    resized_images[i] = cv2.resize(images[i], (128, 128))
-    resized_images[i] = cv2.blur(resized_images[i], (5, 5))
-# split the dataset into train set and test set 75% for train , 12.5% for validation , 12.5 for test
-X_train, X_test, y_train, y_test =  train_test_split(resized_images, proba,train_size=0.75,stratify=proba, random_state=7,shuffle=True)
-# makesure the type is same [pre-processing data]
-X_train = X_train.astype("float32")
-X_test = X_test.astype("float32")
-# Normalize the train set and test set, make sure the pixel value is between 0 and 1.[pre-processing data]
-X_train = X_train / 255
-X_test = X_test / 255
+    resized_images[i] = cv2.resize(images[i], (64,64))
+    resized_images[i] = cv2.GaussianBlur(resized_images[i], (3, 3),0)
+# split the dataset into train set and test set 75% for train , 25% for test
+X_train, X_test, y_train, y_test =  train_test_split(resized_images, proba,train_size=0.75,stratify=proba, random_state=30,shuffle=True)
+
 # Image Enhancement ------- purpose is make the perfomance better, for example, rotation/scaling can reduce the overfiiting and increase the data diversity
-# Use ImageDataGenerator to enhance the image ,there are lots of argument can be changed to influence the performance
-# Each argument's explanation is copied from the tensorflow.org (official website) , simply rotate/shift/flip the image
-Image_data_gen = ImageDataGenerator(
-    rotation_range= 30,     #   random rotation
-    width_shift_range=0.2,  #   shift images horizontally (random shift)
-    height_shift_range=0.2, #   shift images vertically (random shift)
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True #   flip images horizontally (random flip)
-)
+# Contrast Enhancement 
+new_X_train = X_train.copy()
+new_y_train = y_train.copy()
+for x in range(len(new_X_train)):
+   new_X_train[x] = cv2.equalizeHist(X_train[x])
+new_X_train = np.concatenate([X_train, new_X_train])
+new_y_train = np.concatenate([y_train, new_y_train])
+
+# flip(horiton + vertical)
+temp_X = X_train.copy()
+temp_y = y_train.copy()
+for i in range(len(temp_X)):
+    temp_X[i] = cv2.flip(X_train[i],-1)
+new_X_train = np.concatenate([new_X_train, temp_X])
+new_y_train = np.concatenate([new_y_train, temp_y])
+
+#Some random rotation 
+temp_X = X_train.copy()
+temp_y = y_train.copy()
+for i in range(len(temp_X)):
+    rows, cols = X_train[i].shape
+    random_angle = random.randint(-30, 30)
+    rotation_matrix = cv2.getRotationMatrix2D((cols / 2, rows / 2), random_angle, 1)
+    temp_X[i] = cv2.warpAffine(X_train[i], rotation_matrix, (cols, rows))
+new_X_train = np.concatenate([new_X_train, temp_X])
+new_y_train = np.concatenate([new_y_train, temp_y])
+# Now,we got 7968 images for taining; 4524 for class 0; 884 for class 1 ; 320 for class 2 ; 2144 for class 3
+# copy 300 images from class 2
+class_indices = np.where(new_y_train == 2)[0]
+samples_to_copy = np.random.choice(class_indices, size=240, replace=False)
+new_X_train = np.concatenate([new_X_train, new_X_train[samples_to_copy]])
+new_y_train = np.concatenate([new_y_train, new_y_train[samples_to_copy]])
+class_indices = np.where(new_y_train == 1)[0]
+samples_to_copy = np.random.choice(class_indices, size=300, replace=False)
+new_X_train = np.concatenate([new_X_train, new_X_train[samples_to_copy]])
+new_y_train = np.concatenate([new_y_train, new_y_train[samples_to_copy]])
+# Fianlly, add some noise on the image, now we get around 10K train set
+temp_X = X_train.copy()
+temp_y = y_train.copy()
+for i in range(len(temp_X)):
+    noise = np.random.normal(loc=0, scale=10, size=X_train[i].shape)
+    temp_X[i] = np.clip(X_train[i] + noise, 0, 255).astype(np.float32)
+new_X_train = np.concatenate([new_X_train, temp_X])
+new_y_train = np.concatenate([new_y_train, temp_y])
+# make sure all images are float 32
+new_X_train = new_X_train.astype(np.float32)
+X_test = X_test.astype(np.float32)
+# add channel, make sure the image is 4D
+new_X_train = new_X_train.reshape(new_X_train.shape[0], new_X_train.shape[1], new_X_train.shape[2],1)
+# make sure all pixel is  [0,1]
+new_X_train = new_X_train / 255
+X_test = X_test / 255
+
 #reshape the X_train set, make sure it's 4D
-X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2], 1)
-# Fit the image_data_gen
-Image_data_gen.fit(X_train)
-#Method 1 , Use CNN model , and the main idea is coming from Alexnet. Alexnet got 8 layers totally (5 conv layer + 3 fully connect layer)
+#Method 1 , Use CNN model 
 model = Sequential()
-# First Layer , Alexnet uses filters = 96 and the kernel size is 11x11 , stride = 4. 
-# However, I do not think the paramater should be exactly same Alexnet. we use 64 + 7x7 with 2 stride.
-# make sure the input_shape is correct, which is 128x128 with 1 channel (gray-level image).
-# activation= "relu",it's totally same as model.add(Activation(relu'))
-model.add(Conv2D(64, (3, 3),activation='relu',input_shape=(128,128,1)))  
-# For the 2nd layer, Alexnet uses maxpool with size 3x3, but I think 2x2 is better for our task
-#model.add(Conv2D(32, (3, 3),activation='relu'))  
+model.add(Conv2D(32, (3, 3),padding='same',activation='relu',input_shape=(64, 64, 1)))
+model.add(Conv2D(32, (3, 3),padding='same',activation='relu'))
+model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2,2)))
-# Next, it's the 2nd conv layer, Alexnet used 256 and 5x5 as paramater
-# However, I will change it to 128 and 5x5
-#model.add(Conv2D(64,(3,3),activation='relu'))
-model.add(Conv2D(128,(3,3),activation='relu'))
-# Next, add a maxpooling
-#model.add(MaxPooling2D(pool_size=(2,2)))
-# 3rd conv layer , no maxpooling for this convlayer
-#model.add(Conv2D(128,(3,3),activation='relu'))
-model.add(Conv2D(256,(3,3),activation='relu'))
+model.add(Conv2D(64,(3,3),padding='same',activation='relu'))
+model.add(Conv2D(64,(3,3),padding='same',activation='relu'))
+model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2,2)))
-# 4th conv layer , no maxpooling for this convlayer
-#model.add(Conv2D(384,(5,5),activation='relu',padding='same'))
-# the last conv layer , maxpooling needed
-#model.add(Conv2D(256,(5,5),activation='relu',padding='same'))
-#model.add(MaxPooling2D(pool_size=(2,2),strides=2))
-#model.add(Conv2D(256,(5,5),activation='relu',padding='same'))
-#model.add(MaxPooling2D(pool_size=(2,2),strides=2))
-# After the conv layer , Alexnet still got 3 fullyconnect layer
-model.add(Dropout(0.5))
+model.add(Conv2D(128,(3,3),padding='same',activation='relu'))
+model.add(Conv2D(128,(3,3),padding='same',activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
-#model.add(Dense(256,activation="relu"))
-# a large dropout, reduce the overfitting
-# second fully connet
-model.add(Dense(256,activation='relu'))
-# Third fullyconnected layer, for classfier
-#model.add(Dense(4096,activation='relu'))
-model.add(Dropout(0.25))
+model.add(Dense(256,activation='relu', kernel_regularizer=l2(0.01)))
+model.add(Dropout(0.3))
 model.add(Dense(64,activation='relu'))
+model.add(Dropout(0.3))
 model.add(Dense(4,activation="softmax"))
 # After the model built up, we can compile the model
-#opt = SGD(learning_rate=0.001)
-model.compile(optimizer=keras.optimizers.Adam(lr=0.0005), 
-             loss='sparse_categorical_crossentropy',
+model.compile(optimizer=Adam(learning_rate=0.0001), 
+             loss=SparseCategoricalFocalLoss(gamma=2),
              metrics=['accuracy'])
 #fit
-history = model.fit(Image_data_gen.flow(X_train,y_train,batch_size=8),epochs=5,steps_per_epoch=len(X_train)//8,validation_data=(X_test,y_test))
 #model.summary()
+checkpoint = ModelCheckpoint('try_best_model.h5', 
+                             monitor='val_accuracy',  
+                             save_best_only=True,  
+                             mode='max',  #
+                             verbose=1)  
+history = model.fit(new_X_train,new_y_train,batch_size=32, epochs=30,validation_split=0.2,callbacks=[checkpoint])
+#best_model = load_model('best_model.h5')
+# predict
 y_pred = model.predict(X_test)
 predicted_labels = np.argmax(y_pred, axis=1)
-#predicted_class_indices = np.argmax(y_pred, axis=1)
-#one_hot_predictions = to_categorical(predicted_class_indices, num_classes=4)
 y_test = y_test.astype(int)
+# evaluate, f1score / recall /predictions and confusion matrix
 f1_score = classification_report(y_test, predicted_labels)
 confusion  = confusion_matrix(y_test,predicted_labels)
-
+# print the result
 print(f1_score)
 print(confusion)
-plt.plot(history.history['accuracy'])
-plt.plot(history.history["val_accuracy"])
-plt.title("accuracy")
-plt.xlabel("epoch")
-plt.ylabel("accuracy")
-plt.legend(['Train','Test'],loc="upper left")
-plt.show()
+# show the 
+#plt.plot(history.history['accuracy'])
+#plt.plot(history.history["val_accuracy"])
+#plt.title("accuracy")
+#plt.xlabel("epoch")
+#plt.ylabel("accuracy")
+#plt.legend(['Train','Test'],loc="upper left")
+#plt.show()
 
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title("Loss")
-plt.legend(['Train','Test'],loc="upper left")
-plt.show()
+#plt.plot(history.history['loss'])
+#plt.plot(history.history['val_loss'])
+#plt.title("Loss")
+#plt.legend(['Train','Test'],loc="upper left")
+#plt.show()
